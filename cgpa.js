cdd20f2ef0f6898f6576c6e985d851860ebeb8d7
Okolo Yalo Timothy <72364329+timothy123-star@users.noreply.github.com>
2025-07-21 18:45:37 -0700

    select the body element

--- cgpa.js
+++ cgpa.js
@@ -1,5 +1,6 @@
 alert("hellow world");
 
+const selector = document.querySelector("body");
 // document.querySelector("h1").addEventListener("click", (e) => {
 //   e.preventDefault();
 //   document.querySelector("h1").style.backgroundColor = "yellow";
