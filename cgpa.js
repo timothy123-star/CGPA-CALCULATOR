771ab0618979e28fbd85c309d5dfd32577682050
Okolo Yalo Timothy <72364329+timothy123-star@users.noreply.github.com>
2025-07-21 21:49:39 -0700

    Second Commit

--- cgpa.js
+++ cgpa.js
@@ -1,7 +1,7 @@
 alert("hellow world");
 
 const selector = document.querySelector("body");
-// document.querySelector("h1").addEventListener("click", (e) => {
-//   e.preventDefault();
-//   document.querySelector("h1").style.backgroundColor = "yellow";
-// });
+document.querySelector("h1").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("h1").style.backgroundColor = "yellow";
+});
