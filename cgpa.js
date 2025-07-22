a5cd3ac1abf1c9c956aba7ef63f92134256a5479
Okolo Yalo Timothy <72364329+timothy123-star@users.noreply.github.com>
2025-07-21 18:35:31 -0700

    change in the css and js files

---
+++ cgpa.js
@@ -0,0 +1,6 @@
+alert("hellow world");
+
+// document.querySelector("h1").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   document.querySelector("h1").style.backgroundColor = "yellow";
+// });
